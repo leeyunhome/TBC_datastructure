1cf702960aae0f01e27394530433ce9f4e77026e
leeyunhome <leeyunhome@daum.net>
2020-08-15 16:45:52 +0900

    add save_file function

--- Chapter17/Chapter17/main.c
+++ Chapter17/Chapter17/main.c
@@ -2,9 +2,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 //#include <string.h>
 
 #define MAX 30
+#define LMAX 10
 
 struct movie
 {
@@ -22,30 +24,31 @@ void add_an_item();
 void insert_an_item();
 void delete_an_item();
 void delete_all_items();
-void save_file();
+void save_file(struct movie movies[], int n_items);
 void search_by_name();
 void quit();
 
 int main()
 {
-    int choice;
-    int n;
+    size_t n_items = 0;
 
-    struct movie movies[6];
+    struct movie movie_list[LMAX];
 
-    readFILE(movies, &n);
+    readFILE(movie_list, &n_items);
 
     while (1)
     {
+        int choice;
+
         printMenu(&choice);
 
         switch (choice)
         {
         case 1:
-            print_all_items(movies, n);
+            print_all_items(movie_list, n_items);
             break;
         case 2:
-            print_an_item();
+            print_an_item(movie_list, n_items);
             break;
         case 3:
             edit_an_item();
@@ -63,7 +66,7 @@ int main()
             delete_all_items();
             break;
         case 8:
-            save_file();
+            save_file(movie_list, n_items);
             break;
         case 9:
             search_by_name();
@@ -73,7 +76,6 @@ int main()
             break;
         default:
             puts("please input a valid number");
-            break;
         }
     }
 
@@ -101,46 +103,56 @@ int input_int()
     if (input < 1 && input >10)
     {
         puts("please input a valid number");
-        while (getchar() != NULL)
+        while (getchar() != '\n')
             continue;
     }
     return input;
 }
 
-void readFILE(struct movie *movies, int *length)
+void readFILE(struct movie *movies, int *ptr_n_items)
 {
-    char* filename[MAX];
-    int n;
+    char filename[MAX] = { 0, };
+
     int flag;
 
     printf("Please input filename to read and press Enter.\n>>");
 
     flag = scanf("%[^\n]%*c", filename);
+    if (flag != 1)
+    {
+        printf("Wrong input. Terminating.\n");
+        exit(1);
+    }
 
     FILE* fp;
-    fp = fopen(filename, "a+");
+    fp = fopen(filename, "r");
     if (fp == NULL)
     {
         puts("Cannot open file");
         exit(1);
     }
-    
-    fscanf(fp, "%d", &n);
-    printf("\n%d items have been read from the file.\n", n);
-    *length = n;
+    int num;
+    fscanf(fp, "%d%*c", &num);
 
     //char* title[MAX];
 
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i < num; ++i)
     {
-        fscanf(fp, "%[^\n]%*c", &movies[i].title);
-        fscanf(fp, "%f", &movies[i].rating);
+        if (fscanf(fp, "%[^\n]%*c", movies[*ptr_n_items].title) != 1 ||
+            fscanf(fp, "%f%*c", &movies[*ptr_n_items].rating) != 1)
+        {
+            printf("ERROR: Wrong file format.\n");
+            exit(1);
+        }
+        
+        *ptr_n_items += 1;
     }
 
-    for (int movie_index = 0; movie_index < n; ++movie_index)
-    {
-        printf("%d : %s, %f\n", movie_index, movies[movie_index].title, movies[movie_index].rating);
-    }
+    assert(*ptr_n_items == num);
+
+    fclose(fp);
+
+    printf("\n%d items have been read from the file.\n", *ptr_n_items);
 }
 void print_all_items(struct movie* movies, int size)
 {
@@ -173,9 +185,34 @@ void delete_all_items()
 {
     puts("delete_all_items");
 }
-void save_file()
+void save_file(struct movie movies[], int n_items)
 {
-    puts("save_file");
+    char new_filename[MAX] = { 0, };
+    puts("Please input filename to write and press Enter.\n");
+    printf(">> ");
+    if (scanf("%s", new_filename) != 1)
+    {
+        puts("Wrong input. Terminating.");
+        exit(1);
+    }
+    FILE* fw;
+    fw = fopen(new_filename, "w");
+    if (fw == NULL)
+    {
+        printf("ERROR: Cannot open file.\n");
+        exit(1);
+    }
+
+    fprintf(fw, "%d\n", n_items);
+    for (int index = 0; index < n_items; ++index)
+    {
+        fprintf(fw, "%s\n", movies[index].title);
+        fprintf(fw, "%f\n", movies[index].rating);
+    }
+
+    fclose(fw);
+
+    printf("%d items have been saved to the file[%s].\n", n_items, new_filename);
 }
 void search_by_name()
 {
